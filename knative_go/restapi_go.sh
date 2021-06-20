074dcf9393b185143e37aad5b4810f24e8a48869
erasmolpa <erasmolpa@gmail.com>
2021-06-20 20:13:29 +0100

    adding rest api go script

--- knative_go/restapi_go.sh
+++ knative_go/restapi_go.sh
@@ -10,7 +10,7 @@ docker build -t erasmolpa/rest-api-go .
 # Push the container to docker registry
 docker push erasmolpa/rest-api-go 
 
-kn service create helloworld-go --image=docker.io/erasmolpa/rest-api-go --env TARGET="REST Go Sample v1"
+kn service create rest-api-go --image=docker.io/erasmolpa/rest-api-go --env TARGET="REST Go Sample v1"
 
 kubectl get ksvc
 
