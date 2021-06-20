c6ded756875d55343241bf58c501938c03e44e1b
erasmolpa <erasmolpa@gmail.com>
2021-06-20 20:10:02 +0100

    NOT WORKING:  Trying with rest api example.
    NOT WORKING:
    Trying with rest api example.

--- knative_go/restapi_go.sh
+++ knative_go/restapi_go.sh
@@ -2,15 +2,15 @@
 
 set -v
 
-cd helloworld-go
+cd rest-api-go
 
 # Build the container on your local machine
-docker build -t erasmolpa/knative-go:v0 .
+docker build -t erasmolpa/rest-api-go .
 
 # Push the container to docker registry
-docker push erasmolpa/knative-go:v0
+docker push erasmolpa/rest-api-go 
 
-kn service create helloworld-go --image=docker.io/erasmolpa/knative-go:v0 --env TARGET="Go Sample v1"
+kn service create helloworld-go --image=docker.io/erasmolpa/rest-api-go --env TARGET="REST Go Sample v1"
 
 kubectl get ksvc
 
