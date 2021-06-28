282c85d1bb8fd243dfc42c7f1b0c5874d06cfa1d
CristianQS <cristian_31teror@hotmail.com>
2021-06-28 18:43:33 +0100

    Fix hello world service

--- knative_go/6_run_helloworld.sh
+++ knative_go/6_run_helloworld.sh
@@ -5,13 +5,19 @@ set -v
 #cd helloworld-go
 
 # Build the container on your local machine
-#FIXME MAKEFILE docker build -t erasmolpa/knative-go:v0 -f ./helloworld-go/Dockerfile 
+#FIXME MAKEFILE 
+#docker build -t knativeserving/helloworld-go .
 
 # Push the container to docker registry
-#FIXME MAKEFILE docker push erasmolpa/knative-go:v0
+#FIXME MAKEFILE 
 
 kubectl apply --filename ./helloworld-go/service.yaml
 
 #with cli --> kn service create helloworld-go --image=docker.io/erasmolpa/knative-go:v0 --env TARGET="Go Sample v1"
 
 kubectl get ksvc helloworld-go  --output=custom-columns=NAME:.metadata.name,URL:.status.url
+
+kubectl get routes
+kubectl get ksvc
+kubectl get configurations
+kubectl -n knative-serving  get cm config-autoscaler -o yaml
